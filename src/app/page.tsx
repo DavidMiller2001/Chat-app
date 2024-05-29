78628eb17c770237c79feb88c4426a62a68426a2
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-05-29 09:29:05 -0400

    set up shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { SignOutButton } from "@clerk/nextjs";
-import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export default function HomePage() {
   return (
     <main>
       <h1>This is chat app I guess...</h1>
-      <SignOutButton />
+      <SignOutButton>
+        <Button variant={"destructive"}>Sign Out</Button>
+      </SignOutButton>
     </main>
   );
 }
