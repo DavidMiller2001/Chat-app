923556e4ebc0831101d93556b1e7ce5d1b16e143
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-05-29 09:25:09 -0400

    set up clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "Create T3 App",
@@ -14,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body className="flex h-screen items-center justify-center bg-slate-700">
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
